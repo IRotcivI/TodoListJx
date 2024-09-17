9ffe13a5c87348d1e7caffc130491a8e27a89c48
FAYE <fayedaour@outlook.fr>
2024-09-17 15:23:14 +0200

    "En cour" Systéme erreur & sout pour les bouttons

--- TodoListJx/src/main/java/appli/todolistjx/HelloController.java
+++ TodoListJx/src/main/java/appli/todolistjx/HelloController.java
@@ -8,7 +8,27 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
+    private Label errorText;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        System.out.println(onHelloButtonClick());
     }
+
+    @FXML
+    protected void onErrorButton()
+    {
+        if (errorText.getText().isEmpty())
+        {
+            errorText.setText("Erreur!");
+        }
+        else
+        {
+            errorText.setText("");
+        }
+        System.out.println(onErrorButton(););
+    }
+
+
 }
